54d7978909f97210a2f249a83f4345e0a98eaec9
Yujie Bian <artjay@163.com>
2024-04-10 15:37:08 +0800

    delete old files

--- public/js/slide.js
+++
@@ -1,9 +0,0 @@
-var target = document.getElementById("fixedHeader");
-window.addEventListener("scroll", function () {
-    var scrollHeight = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    if (scrollHeight > 150) {
-        target.style.top = "0";
-    } else {
-        target.style.top = "-100px";
-    }
-});
